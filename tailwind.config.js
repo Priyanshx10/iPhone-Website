c897b2095274e7594346fa1a8cb2ad9101fa0596
Priyansh <Priyanshyadav1012@gmail.com>
2024-06-22 10:49:47 +0530

    NavBar is in Progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#2997FF',
+        gray: {
+          default: "#86868B",
+          100: '#98928d',
+          200: '#afafaf',
+          300: '#42424570'
+        },
+        zinc: '#101010'
+      }
+    },
   },
   plugins: [],
 }
