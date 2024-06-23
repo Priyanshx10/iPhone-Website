56f8906853d969e1b847fd8fa6ebd007e61f154c
Priyansh <Priyanshyadav1012@gmail.com>
2024-06-23 13:03:54 +0530

    Implement Gsao Animations.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,7 @@ import { navLists } from "../assets/constants"
 const NavBar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
-      <nav className="flex w-full screen-max-width
-">
+      <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple Logo" width={14} height={18}/>
       </nav>
 
