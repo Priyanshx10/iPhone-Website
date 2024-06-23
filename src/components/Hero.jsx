daf13d3e0367ea01473c73aee7c21dd88848645d
Priyansh <Priyanshyadav1012@gmail.com>
2024-06-23 13:27:07 +0530

    Complete the Hero Animations.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,9 @@ const Hero = () => {
     }
   }, [])
   useGSAP(() => {
-    gsap.to('#hero',{opacity: 1, delay: 1.5})
+    gsap.to('#hero',{opacity: 1, delay: 1.5}),
+    gsap.to('#cta',{opacity: 1, delay: 2,  y: -50})
+
   },[])
   
 
@@ -34,9 +36,13 @@ const Hero = () => {
         <video className=" pointer-events-none "autoPlay muted playsInline={true} key={videoSrc}>
           <source src={videoSrc} type="video/mp4"/>
         </video>
-
+      </div>
       </div>
 
+      <div id= "cta" 
+        className="flex flex-col items-center opacity-0 translate-y-20">
+            <a href="#highlights" className="btn">Buy</a>
+            <p className="font-normal text-xl">From $199/month pr $999</p>
       </div>
     </section>
   )
