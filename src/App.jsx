cbfc1de77ce572902c73ece7d2343b36b5fc2214
Priyansh <Priyanshyadav1012@gmail.com>
2024-06-27 19:52:48 +0530

    Completed FrontEnd

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Model from './components/Model';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import * as Sentry from '@sentry/react';
 
@@ -13,6 +14,7 @@ import * as Sentry from '@sentry/react';
 const App = () => {
   return (
    <main >
+      <SpeedInsights />
       <NavBar />
       <Hero/>
       <HighLight />
